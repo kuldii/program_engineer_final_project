e15dc58442a2345870fad63cc407c60a21d56f9b
LevShlyogin <147638117+LevShlyogin@users.noreply.github.com>
2024-01-16 21:45:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Импортируем библиотеки
 import io
 import streamlit as st
 from PIL import Image
@@ -5,23 +6,42 @@ from transformers import BlipProcessor, BlipForConditionalGeneration
 
 
 def load_image(uploaded_file):
+    """
+    Функция загружает изображение из переданного файла и отображает его с помощью библиотеки streamlit.
+    
+    Parameters:
+    - uploadedFile: BytesIO
+        Файл, содержащий изображение.
+    
+    Returns:
+    - img: Image
+        Объект изображения Image из библиотеки PIL (Python Imaging Library), сконвертированный в формат RGB.
+        Если uploadedFile равен None, возвращается значение None.
+    """
     if uploaded_file is not None:
+        # Получаем данные изображения из файла
         image_data = uploaded_file.getvalue()
+        
+        # Отображаем изображение с помощью библиотеки streamlit
         st.image(image_data, width=200)
+
+        # Отображаем название загруженного изображения
         st.write("Image Input : ", uploaded_file.name)
+         # Возвращаем объект изображения Image из библиотеки PIL (Python Imaging Library), сконвертированный в формат RGB
         return Image.open(io.BytesIO(image_data)).convert('RGB')
     else:
         return None
 
-
+# Инициализация процессора для обработки изображений и генерации текстовых описаний
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
 
-
+# Функция для загрузки модели для генерации текстовых описаний на основе изображений
+# Результат функции кешируется для повторного использования
 @st.cache
 def load_model():
     return BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 
-
+# Загрузка модели
 model = load_model()
 
 st.title("Final Project - Image to Text")
@@ -35,9 +55,11 @@ st.write("""
 
 st.write("""#### Our Project""")
 
+# Загрузка изображения
 uploadedFile = st.file_uploader('Upload image here')
 raw_image = load_image(uploadedFile)
 
+# Обработка изображения и генерация текстового описания при нажатии кнопки Submit
 result = st.button('Submit')
 
 if result:
